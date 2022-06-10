9a7605cf6705f986067f7e9c75d18f72acc042e9
rich-ter <me@philipposrichter.com>
2022-06-10 20:43:52 +0300

    adding into github

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import {useState} from 'react' 
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import Task from './components/Task'
+import AddTask from './components/AddTask'
+
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -24,10 +27,28 @@ function App() {
     }
 ])
 
+
+// Add task
+  const addTask = (task) => {
+    console.log(task)
+  }
+
+// Delete task
+  const deleteTask = (id) =>{
+    setTasks(tasks.filter((task)=>task.id !== id))    
+  }
+
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+  }
+
+
   return (
     <div className="container">
       <Header/>
-      <Tasks tasks={tasks}/>
+      <AddTask onAdd = {addTask}/>
+      {tasks.length > 0 ? (
+      <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> ) : ( 'No tasks' )}
     </div>
   );
 }
